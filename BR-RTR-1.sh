1a70ee17d62109ed7b7f4956602fd4ee74ad0e95
S0faK1ng <carshinruslano@yandex.ru>
2025-05-01 05:31:01 +0300

    Update BR-RTR-1.sh
    fix timesyncd

--- BR-RTR-1.sh
+++ BR-RTR-1.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
+# Создаем необходимые директории для сетевых интерфейсов
 mkdir /etc/net/ifaces/ens19
 mkdir /etc/net/ifaces/iptunnel
 
+# Настройка ethernet-интерфейса ens19
 cat <<EOF > /etc/net/ifaces/ens19/options
 BOOTPROTO=static
 TYPE=eth
-CONFIG_WIRELESS=no
+CONFIG_WIRELANDS=no
 SYSTEMD_BOOTPROTO=dhcp4
 CONFIG_IPV4=yes
 DISABLED=no
@@ -12,35 +16,31 @@ NM_CONTROLLED=no
 SYSTEMD_CONTROLLED=no
 EOF
 
+# IP адрес ethernet-интерфейса ens19
 cat <<EOF > /etc/net/ifaces/ens19/ipv4address
-
 192.168.4.1/28
-
 EOF
 
+# Настройки GRE-туннеля
 cat <<EOF > /etc/net/ifaces/iptunnel/ipv4address
-
 10.0.0.2/28
-
 EOF
 
 cat <<EOF > /etc/net/ifaces/iptunnel/options
-
 TYPE=iptun
 TUNTYPE=gre
 TUNLOCAL=172.16.5.2
 TUNREMOTE=172.16.4.2
 TUNOPTIONS='ttl 64'
-
 EOF
 
+# Маршруты через туннель
 cat <<EOF > /etc/net/ifaces/iptunnel/ipv4route
-
 192.168.1.0/24 via 10.0.0.1
 192.168.2.0/24 via 10.0.0.1
-
 EOF
 
+# Настраиваем sysctl для IPv4 forwarding и безопасности сети
 cat <<EOF > /etc/net/sysctl.conf
 net.ipv4.ip_forward = 1
 net.ipv4.conf.default.rp_filter = 1
@@ -49,43 +49,52 @@ net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_timestamps = 0
 EOF
 
+# Применяем изменения sysctl
 sysctl -p
 
+# Очищаем правила iptables и настраиваем NAT
 iptables -F
 iptables -t nat -A POSTROUTING -o ens18 -j MASQUERADE
 iptables-save > /etc/sysconfig/iptables
 
+# Перезагружаем сеть
 service network restart
 
-# Добавляем IPTABLES в автозапуск
+# Включаем службу iptables
 systemctl enable iptables
 systemctl start iptables
 
+# Создаем администратора сети
 useradd net_admin -m
-passwd net_admin
+passwd net_admin # Здесь введите пароль для нового пользователя
 
-# Добавляем пользователя в sudoers
+# Даем права sudo новому пользователю
 echo 'net_admin ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
 
+# Удаляем старые сервисы синхронизации времени
 apt-get remove ntp
-
 apt-get remove chrony
 
+# Обновляем список пакетов
 apt-get update
 
+# Устанавливаем новый сервис синхронизации времени
 apt-get install systemd-timesyncd
 
-nano /etc/systemd/timesyncd.conf
-
+# Конфигурируем timesyncd
+cat <<EOF > /etc/systemd/timesyncd.conf
 [Time]
 NTP=172.16.4.2
+EOF
 
+# Запускаем и включаем timesyncd
 systemctl enable --now systemd-timesyncd
 
+# Обновляем репозитории и устанавливаем OpenSSH сервер
 apt-get update
-
 apt-get install openssh-server
 
+# Настраиваем SSH-сервер
 cat <<EOF > /etc/openssh/sshd_config
 Port 22
 MaxAuthTries 2
@@ -94,14 +103,14 @@ PermitRootLogin no
 Banner /root/banner
 EOF
 
+# Баннер авторизации
 cat <<EOF > /root/banner
 Authorized access only
-
 EOF
 
-
+# Активируем и перезапускаем SSH-сервис
 systemctl enable --now sshd
 systemctl restart sshd
 
-# Переименовываем машину
+# Меняем имя хоста машины
 hostnamectl set-hostname br-rtr.au-team.irpo
