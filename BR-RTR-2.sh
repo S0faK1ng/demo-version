0c58550efe1f01e69c9468cc9af0ed151658f572
S0faK1ng <carshinruslano@yandex.ru>
2025-05-12 14:05:59 +0300

    Update BR-RTR-2.sh

--- BR-RTR-2.sh
+++ BR-RTR-2.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+#устанавливаем ipsec
+apt-get update && apt-get install -y strongswan
+
+#редактируем ipsec
+echo "редактируем ipsec.conf"
+сat <<EOF > /etc/strongswan/ipsec.conf
+
+config setup
+
+conn gre
+	auto=start
+	type=tunnel
+	authby=secret
+	left=10.0.0.2
+	right=10.0.0.1
+	leftsubnet=0.0.0.0/0
+	rightsubnet=0.0.0.0/0
+	leftprotoport=gre
+	rightprotoport=gre
+	ike=aes256-sha2_256-modp1024!
+	esp=aes256-sha2_256!
+EOF
+
+#редактируем ipsec.secrets
+echo "редактируем ipsec.secrets"
+сat <<EOF > /etc/strongswan/ipsec.secrets
+	10.0.0.1 10.0.0.2  : PSK "P@ssw0rd"
+EOF
+
 iptables -t nat -A PREROUTING -p tcp -d 192.168.4.1 --dport 80 -j DNAT --to-destination 192.168.4.10:8080
 iptables-save > /etc/sysconfig/iptables
 iptables -t nat -A PREROUTING -p tcp -d 192.168.4.1 --dport 2024 -j DNAT --to-destination 192.168.4.10:2024
