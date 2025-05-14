744bb0a9ca79f04120a6011a71af7a81c74d89a1
S0faK1ng <carshinruslano@yandex.ru>
2025-05-14 16:55:35 +0300

    Update BR-RTR-2.sh

--- BR-RTR-2.sh
+++ BR-RTR-2.sh
@@ -34,29 +34,6 @@ echo "*.* @@192.168.1.10:514" > /etc/rsyslog.d/all_log.conf
 systemctl enable --now rsyslog
 systemctl restart rsyslog
 
-iptables -F
-iptables -t nat -F
-iptables -t mangle -F
-iptables -X
-iptables -P INPUT   DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT  ACCEPT
-iptables -A INPUT  -i lo -j ACCEPT
-iptables -A INPUT  -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i "ens19" -s "192.168.3.0/27" -j ACCEPT
-iptables -A INPUT   -p icmp -j ACCEPT
-iptables -A FORWARD -p icmp -j ACCEPT
-iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
-iptables -A FORWARD -p tcp --dport 443 -j ACCEPT
-iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
-iptables -A FORWARD -p udp --dport 53 -j ACCEPT
-iptables -A FORWARD -p tcp --dport 53 -j ACCEPT
-iptables -A FORWARD -p udp --dport 123 -j ACCEPT
-iptables -t nat -A POSTROUTING -o "ens18" -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp -d 192.168.3.1 --dport 80 -j DNAT --to-destination 192.168.3.10:8080
+iptables -t nat -A PREROUTING -p tcp -d 192.168.3.1 --dport 2024 -j DNAT --to-destination 192.168.3.10:2024
 iptables-save > /etc/sysconfig/iptables
-
-
-#iptables -t nat -A PREROUTING -p tcp -d 192.168.3.1 --dport 80 -j DNAT --to-destination 192.168.3.10:8080
-#iptables -t nat -A PREROUTING -p tcp -d 192.168.3.1 --dport 2024 -j DNAT --to-destination 192.168.3.10:2024
-#iptables-save > /etc/sysconfig/iptables
